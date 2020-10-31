cfe0c7fb488c84235361ef0e8efe78d1a5ca953f
zyt19981201 <1479297914@qq.com>
2020-10-31 11:50:55 +0800

    导航栏路由跳转

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h2>2222</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
