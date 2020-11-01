68222f487a0c285a91452d4835f6dcaefebe5b26
zyt19981201 <1479297914@qq.com>
2020-11-01 17:08:58 +0800

    轮播图与推荐

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,31 @@
+<template>
+  <swiper>
+    <swiper-item v-for="banner in banners" :key="banner.image">
+      <a :href="banner.link">
+        <img :src="banner.image" alt="" />
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from "components/common/swiper";
+export default {
+  name: "HoemSwiper",
+  props: {
+    banners: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  components: {
+    Swiper,
+    SwiperItem,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="recommend">
+    <div
+      v-for="recommend in recommends"
+      :key="recommend.image"
+      class="recommend-item"
+    >
+      <a :href="recommend.link">
+        <img :src="recommend.image" alt="" />
+        <div>{{ recommend.title }}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.recommend {
+  display: flex;
+  text-align: center;
+  font-size: 12px;
+  padding: 10px 0px 10px;
+  border-bottom: 10px solid #eee;
+}
+.recommend-item {
+  flex: 1;
+}
+.recommend-item img {
+  width: 70px;
+  height: 70px;
+  margin-bottom: 10px;
+}
+</style>
