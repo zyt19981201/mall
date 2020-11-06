5f41f616c689ef551e27526d9b12679bea2fceba
zyt19981201 <1479297914@qq.com>
2020-11-06 16:26:08 +0800

    完成详情页轮播图功能

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,24 +1,38 @@
 <template>
   <div id="detail">
     <detail-nav-bar />
+    <detail-swiper :top-images="topImages" />
   </div>
 </template>
 
 <script>
 import DetailNavBar from "views/detail/childComps/DetailNavBar";
+import DetailSwiper from "./childComps/DetailSwiper";
+
+import { getDetail } from "../../network/detail";
 
 export default {
   name: "Detail",
   components: {
     DetailNavBar,
+    DetailSwiper,
   },
   data() {
     return {
       iid: null,
+      topImages: [],
     };
   },
   created() {
+    // 1.保存iid
     this.iid = this.$route.params.iid;
+
+    // 2.根据iid请求详情数据
+    getDetail(this.iid).then((res) => {
+      console.log(res);
+      // 1.获取顶部轮播图图片
+      this.topImages = res.result.itemInfo.topImages;
+    });
   },
 };
 </script>
