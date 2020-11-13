5b8ed3d4e2700e73db31bfda30fb64808534006d
zyt19981201 <1479297914@qq.com>
2020-11-13 18:16:01 +0800

    添加fastclick

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import toast from "components/common/toast/index"
+import FastClick from 'fastclick'
 
 Vue.prototype.$bus = new Vue()
 
 Vue.use(toast)
+
+// 解决移动端300毫秒点击延迟
+FastClick.attach(document.body)
 
 new Vue({
   el: '#app',
