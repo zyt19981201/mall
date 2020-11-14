f2a2391b4d3a228bccd9f457e1cb1e91fe89fafb
zyt19981201 <1479297914@qq.com>
2020-11-14 16:00:48 +0800

    老师项目全部完成

--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import router from './router/index'
 import store from './store/index'
 import toast from "components/common/toast/index"
 import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.prototype.$bus = new Vue()
 
 Vue.use(toast)
+Vue.use(VueLazyLoad, {
+  loading: require('assets/img/common/zw.jpg')
+})
 
 // 解决移动端300毫秒点击延迟
 FastClick.attach(document.body)
